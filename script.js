1c96cafa8fe784c40858a3e0a53ca38c0d572993
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-28 08:50:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const byteSize = (str) => {
-  // write your code here
+   // Create a Blob object with the input string
+  const blob = new Blob([str]);
+
+  // Get the size property of the Blob object (size in bytes)
+  return blob.size;
 };
 
 // Do not change the code below
